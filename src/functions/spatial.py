5619eafc52e6287d7e9ccfc7ba222fa0d9f76e2c
Aaron Sanchez <aaroncz032@gmail.com>
2025-11-23 14:27:28 -0600

    docs: Function description

--- src/functions/spatial.py
+++ src/functions/spatial.py
@@ -2,6 +2,13 @@ import math
 from src.structures import Manifestation
 
 def euclidean_distance(m1: Manifestation, m2: Manifestation) -> float:
+  """
+  Euclidean distance between two manifestations.
+
+  :param m1: First manifestation.
+  :param m2: Second manifestation.
+  :return: Euclidean distance.
+  """
   dx = m1.x - m2.x
   dy = m1.y - m2.y
   return math.hypot(dx, dy)
