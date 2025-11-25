ea38b41cea7ffeb36de064640c1697f75abac340
Aaron Sanchez <aaroncz032@gmail.com>
2025-11-24 21:08:54 -0600

    refactor: func names, func changed.

--- src/functions/scores.py
+++ src/functions/scores.py
@@ -4,7 +4,7 @@ from src.functions.general import min_max_normalize
 from src.functions.geothermometers import compute_geothermometers
 from src.structures import NormalizationStats
 
-def compute_temperature_score(temp: float, stats: NormalizationStats) -> float:
+def temperature_score(temp: float, stats: NormalizationStats) -> float:
   """
   Compute T_score(i)
 
@@ -14,7 +14,7 @@ def compute_temperature_score(temp: float, stats: NormalizationStats) -> float:
   """
   return min_max_normalize(temp, stats.temp_min, stats.temp_max)
 
-def compute_geochemical_score(
+def geochemical_score(
   si: float,
   na: float,
   k: float,
@@ -35,7 +35,7 @@ def compute_geochemical_score(
   t_avg = (t_si + t_nak + t_nakca) / 3.0
   return min_max_normalize(t_avg, stats.tavg_min, stats.tavg_max)
 
-def compute_spatial_score(
+def spatial_score(
   temp_interp: float,
   cond_interp: float,
   cl_interp: float,
@@ -54,34 +54,3 @@ def compute_spatial_score(
   cond_norm = min_max_normalize(cond_interp, stats.cond_min, stats.cond_max)
   cl_norm = min_max_normalize(cl_interp, stats.cl_min, stats.cl_max)
   return (t_norm + cond_norm + cl_norm) / 3.0
-
-def compute_objective_F(
-  temp: float,
-  cond: float,
-  cl: float,
-  si: float,
-  na: float,
-  k: float,
-  ca: float,
-  stats: NormalizationStats,
-  weights: Tuple[float, float, float] = (1.0, 1.0, 1.0)
-) -> float:
-  """
-  :param temp: Temperature.
-  :param cond: Conductivity.
-  :param cl: Chloride.
-  :param si: Silica concentration.
-  :param na: Sodium concentration.
-  :param k: Potassium concentration.
-  :param ca: Calcium concentration.
-  :param stats: Global normalization statistics.
-  :param weights for the temperature, geochemical and spatial components.
-  :return: Objective value F(i).
-  """
-  w1, w2, w3 = weights
-
-  t_score = compute_temperature_score(temp, stats)
-  g_score = compute_geochemical_score(si, na, k, ca, stats)
-  s_score = compute_spatial_score(temp, cond, cl, stats)
-
-  return w1 * t_score + w2 * g_score + w3 * s_score
